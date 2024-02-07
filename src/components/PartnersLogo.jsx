7d3012ab3474830e9b59e776ceb011d616dd936e
Bishech <bishechhh@mail.ru>
2024-02-07 13:49:51 +0300

    add PartnersLogo

---
+++ src/components/PartnersLogo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import partnersItem from "../assets/partners-item.png";
+
+export const PartnersLogo = () => {
+  return (
+    <div>
+      <img src={partnersItem} />
+    </div>
+  );
+};
