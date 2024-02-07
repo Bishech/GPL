bd3aeefe4980a9698b3b62c2be3bafc0d06d2f2f
Bishech <bishechhh@mail.ru>
2024-02-07 13:49:17 +0300

    refactoring PlayerCard

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -11,10 +11,10 @@ export const PlayerCard = ({
   return (
     <div className="flex gap-10">
       <div className="w-1/2 flex flex-col justify-center items-center">
-        <p className="bg-white text-blue-800 text-center text-5xl w-72 h-16 flex items-center justify-center skew-x-[-5deg] shadow-lg shadow-black">
+        <img src={playerImg} className="w-2/3" />
+        <p className="-mt-28 bg-white text-blue-800 text-center text-5xl w-56 h-16 flex items-center justify-center skew-x-[-5deg] shadow-lg shadow-black">
           {playerName}
         </p>
-        <img src={playerImg} className="w-2/3" />
       </div>
       <div className="flex flex-col justify-stretch w-1/2 content-center ">
         <div className="grid grid-col-6 gap-4 content-center justify-stretch">
