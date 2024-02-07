0864a1166e1c30e46c8b057738a03610224138e2
Bishech <bishechhh@mail.ru>
2024-02-07 13:50:07 +0300

    add PartnersProLogo

---
+++ src/components/PartnersProLogo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import partnersProItem from "../assets/partners-pro-item.png";
+
+export const PartnersProLogo = () => {
+  return (
+    <div>
+      <img src={partnersProItem} />
+    </div>
+  );
+};
