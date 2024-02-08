0b6b595811732d1ebb9264bbb9b96a5e44610e5d
Bishech <bishechhh@mail.ru>
2024-02-08 15:29:50 +0300

    fix Team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -13,14 +13,14 @@ export const Team = ({ logo, name, points, group }) => {
     <div className=" text-center">
       <button
         disabled
-        className="shadow-lg shadow-slate-900 skew-x-[-5deg]  cursor-default bg-gray-700 border-2 rounded-lg w-2/3 h-44 border-black text-white"
+        className="skew-x-[-5deg]  cursor-default bg-gray-700 border-2 rounded-lg w-2/3 h-44 border-black text-white"
       >
         <div
-          className={`before:w-6 before:rounded before:content-[''] before:h-40 before:-top-3  before:absolute before:left-0 before:border-t-[12px]  ${groupColor[group]} w-full h-full border-2  border-black rounded-lg border-b-[12px] border-b-slate-800 border-t-[12px] border-t-slate-600 relative border-x-transparent`}
+          className={`before:w-6 before:rounded before:content-[''] before:h-40 before:-top-[17px]  before:absolute before:left-0 before:border-t-[18px]  ${groupColor[group]} w-full h-full border-2  border-black rounded-lg border-b-[18px] border-b-slate-800 border-t-[18px] border-t-slate-600 relative border-x-transparent`}
         >
           <div className="w-full h-full flex flex-row gap-4 px-6 justify-center items-center text-white">
             <div>
-              <img src={logo} className="w-40 h-40 object-cover" />
+              <img src={logo} className="w-36 h-36 object-cover" />
             </div>
             <div className="text-8xl">{name}</div>
             <div className="flex flex-grow"></div>
