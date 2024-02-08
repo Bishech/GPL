d0688c49dddffad8c22a69b85283ddb9a8a9f75b
Bishech <bishechhh@mail.ru>
2024-02-08 15:29:32 +0300

    fix GameData

--- src/components/GameData.jsx
+++ src/components/GameData.jsx
@@ -10,12 +10,12 @@ export const GameData = ({
     <div className="text-center">
       <button
         disabled
-        className="shadow-lg shadow-slate-900 skew-x-[-5deg] cursor-default bg-gray-700 border-2 rounded-lg w-2/3 h-44 border-black"
+        className="skew-x-[-5deg] cursor-default bg-gray-700 border-2 rounded-lg w-2/3 h-44 border-black"
       >
-        <div className="h-full w-full border-2  border-black rounded-lg border-b-[12px] border-b-slate-800 border-t-[12px] border-t-slate-600 relative border-x-transparent">
+        <div className="h-full w-full border-2  border-black rounded-lg border-b-[18px] border-b-slate-800 border-t-[18px] border-t-slate-600 relative border-x-transparent">
           <div className="w-full h-full flex flex-row gap-10 px-6 justify-center items-center">
             <div>
-              <img src={firstTeamLogo} className="w-36 h-36 object-cover" />
+              <img src={firstTeamLogo} className="w-32 h-32 object-cover" />
             </div>
             <div className="flex flex-grow"></div>
             <div className="text-9xl text-white">{firstTeamPoints}</div>
@@ -23,7 +23,7 @@ export const GameData = ({
             <div className="text-9xl text-red-600">{secondTeamPoints}</div>
             <div className="flex flex-grow"></div>
             <div>
-              <img src={secondTeamLogo} className="w-36 h-36 object-cover" />
+              <img src={secondTeamLogo} className="w-32 h-32 object-cover" />
             </div>
           </div>
         </div>
