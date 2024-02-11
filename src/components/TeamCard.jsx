e230fbfb52c3b2b912bdea0fbfb97f8b0b3d3068
Bishech <bishechhh@mail.ru>
2024-02-12 00:38:38 +0300

    fix TeamCard

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { Player } from "./Player";
 
-export const TeamCard = ({ logo, players }) => {
+export const TeamCard = ({ logo, players, name }) => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-5 -mt-10">
-      <div className="flex flex-row items-center gap-20">
+      <div className="flex flex-row justify-between  items-center gap-32">
         <img src={logo} className="w-72 h-72" />
+        <h1 className="text-8xl text-white">{name}</h1>
       </div>
       <div className="flex flex-row gap-20">
-        {players.map((player) => {
-          return (
-            <Player
-              playerImg={player.img}
-              playerName={player.name}
-              key={player.id}
-            />
-          );
-        })}
+        {players.map((player) => (
+          <Player
+            playerImg={player.img}
+            playerName={player.name}
+            teamName={name}
+          />
+        ))}
       </div>
     </div>
   );
