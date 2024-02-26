64af452bf6d0d7fab1e6ac81254b534d131e263b
Bishech <bishechhh@mail.ru>
2024-02-26 21:14:34 +0300

    add sign in button to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,23 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div className="w-screen h-14 bg-neutral-900 flex fixed left-0 right-0 z-20 top-0">
-      <Link
-        to="/"
-        className="flex flex-row justify-start gap-4 items-center ml-7"
-      >
-        <img src={logo} className="h-10 w-10" />
-        <p className="text-xl text-white font-extra">GPL SEASON 7</p>
-      </Link>
+    <div className="w-screen h-14 bg-neutral-900 fixed left-0 right-0 z-20 top-0">
+      <div className="w-full h-full flex items-center justify-between">
+        <div>
+          <Link
+            to="/"
+            className="flex flex-row justify-start gap-4 items-center ml-7"
+          >
+            <img src={logo} className="h-10 w-10" />
+            <p className="text-xl text-white font-extra">GPL SEASON</p>
+          </Link>
+        </div>
+        <button className="px-8 text-xl skew-x-[-5deg] font-extra relative top-4 h-12 bg-neutral-900 hover:bg-neutral-800 border-2 rounded-lg shadow-md shadow-black border-yellow-400 text-white flex justify-end items-center mr-14">
+          <div>
+            <div>Войти</div>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
