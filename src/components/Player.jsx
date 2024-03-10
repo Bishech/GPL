a7349c340d74b5cfe356e367535e3d23469feebc
Bishech <bishechhh@mail.ru>
2024-03-10 13:22:50 +0300

    add responsive design to the TeamCard

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,8 +5,8 @@ export const Player = ({ playerImg, playerName, teamName }) => {
   return (
     <Link to={`/player/${teamName}/${playerName}`}>
       <div className="flex flex-col items-center">
-        <img src={playerImg} className="w-72 h-72" />
-        <p className="bg-white text-blue-800 text-center text-2xl w-40 h-8 skew-x-[-5deg] shadow-lg shadow-black -mt-16">
+        <img src={playerImg} className="w-72 h-72 object-contain" />
+        <p className="bg-white text-blue-800 text-center text-2xl w-40 skew-x-[-5deg] shadow-lg shadow-black -mt-20 md:-mt-16 text-wrap">
           {playerName}
         </p>
       </div>
--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -4,11 +4,13 @@ import { Player } from "./Player";
 export const TeamCard = ({ logo, players, name }) => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-5 -mt-10">
-      <div className="flex flex-row justify-between  items-center gap-32">
-        <img src={logo} className="w-72 h-72" />
-        <h1 className="text-8xl text-white">{name}</h1>
+      <div className="flex lg:flex-row flex-col justify-between items-center lg:gap-32">
+        <img src={logo} className="lg:w-64 md:w-60 w-48 lg:h-64 md:h-60 h-48" />
+        <h1 className="lg:text-8xl md:text-7xl text-5xl text-white uppercase">
+          {name}
+        </h1>
       </div>
-      <div className="flex flex-row gap-20">
+      <div className="flex md:flex-row flex-col md:gap-20 gap-6">
         {players.map((player) => (
           <Player
             playerImg={player.img}
