65c5eeca9ae4978ac0103101cd7e65152af1ba0e
Bishech <bishechhh@mail.ru>
2024-03-23 21:48:25 +0300

    fix design in TeamCard

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -6,11 +6,11 @@ export const TeamCard = ({ logo, players, name, id }) => {
     <div className="w-full flex flex-col justify-center items-center gap-5 -mt-10">
       <div className="flex lg:flex-row flex-col justify-between items-center lg:gap-32">
         <img src={logo} className="lg:w-64 md:w-60 w-48 lg:h-64 md:h-60 h-48" />
-        <h1 className="lg:text-8xl md:text-7xl text-5xl text-white uppercase">
+        <h1 className="lg:text-8xl md:text-7xl text-5xl text-center text-white uppercase">
           {name}
         </h1>
       </div>
-      <div className="flex md:flex-row flex-col gap-20">
+      <div className="flex md:flex-row flex-col items-center justify-center flex-wrap gap-20">
         {players.map((player) => (
           <Player
             playerImg={player.img}
