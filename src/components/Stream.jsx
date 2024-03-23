0fa248945e92d7b134d06b15ae9522fe619e223a
Bishech <bishechhh@mail.ru>
2024-03-23 20:30:58 +0300

    init youtube-react

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -1,12 +1,26 @@
-import { TwitchEmbed } from "react-twitch-embed";
+// import { TwitchEmbed } from "react-twitch-embed";
+
+// import YouTube from "react-youtube";
 
 export const Stream = () => {
-  return (
-    <TwitchEmbed
-      channel="krifor69"
-      autoplay={false}
-      hideControls
-      withChat={false}
-    />
-  );
+  // const opts = {
+  //   height: "450",
+  //   width: "700",
+  //   playerVars: {
+  //     autoplay: 0,
+  //   },
+  // };
+  // const onPlayerReady = (event) => {
+  //   event.target.pauseVideo();
+  // };
+  // return (
+  //   <YouTube opts={opts} onReady={onPlayerReady} videoId={"vxU8i7d6ues"} />
+  // Twitch
+  // <TwitchEmbed
+  //   channel="krifor69"
+  //   autoplay={false}
+  //   hideControls
+  //   withChat={false}
+  // />
+  // );
 };
