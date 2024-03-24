1d83be2ee8e99a0d77aacfb10f71b3d09e911260
Bishech <bishechhh@mail.ru>
2024-03-24 19:47:43 +0300

    fix group color in Team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,10 +3,10 @@ import logo_gpl from "../assets/logo-gpl.svg";
 import { Link } from "react-router-dom";
 
 const groupColor = {
-  a: "before:border-t-blue-500 before:border-b-blue-700 before:bg-blue-600",
-  b: "before:border-t-red-500 before:border-b-red-700 before:bg-red-600",
-  c: "before:border-t-green-500 before:border-b-green-700 before:bg-green-600",
-  d: "before:border-t-purple-500 before:border-b-purple-700 before:bg-purple-600",
+  A: "before:border-t-blue-500 before:border-b-blue-700 before:bg-blue-600",
+  B: "before:border-t-red-500 before:border-b-red-700 before:bg-red-600",
+  C: "before:border-t-green-500 before:border-b-green-700 before:bg-green-600",
+  D: "before:border-t-purple-500 before:border-b-purple-700 before:bg-purple-600",
 };
 
 export const Team = ({ logo, name, points, group }) => {
