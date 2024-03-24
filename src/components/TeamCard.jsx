31772e11db72570a80fada2e35df1ca6d70d7bda
Bishech <bishechhh@mail.ru>
2024-03-25 01:21:19 +0300

    fix logo in TeamCard

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -5,7 +5,10 @@ export const TeamCard = ({ logo, players, name, teamId }) => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-5 -mt-10">
       <div className="flex lg:flex-row flex-col justify-between items-center lg:gap-32">
-        <img src={logo} className="lg:w-64 md:w-60 w-48 lg:h-64 md:h-60 h-48" />
+        <img
+          src={logo}
+          className="lg:w-64 md:w-60 w-48 lg:h-64 md:h-60 h-48 object-contain"
+        />
         <h1 className="lg:text-8xl md:text-7xl text-5xl text-center text-white uppercase">
           {name}
         </h1>
