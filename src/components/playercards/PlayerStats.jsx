c4fd9f865c82e24277fea8c273789769a0010594
Bishech <bishechhh@mail.ru>
2024-03-25 23:44:03 +0300

    fix design in PlayerStats

--- src/components/playercards/PlayerStats.jsx
+++ src/components/playercards/PlayerStats.jsx
@@ -10,7 +10,7 @@ export const PlayerStats = ({
   winRate,
 }) => {
   return (
-    <div className="pt-3 md:grid md:grid-row-7 flex flex-col gap-4 md:items-center content-center">
+    <div className="pt-3 md:grid md:grid-row-7 flex flex-col gap-4 md:items-start content-center">
       <div className="md:text-7xl text-4xl col-span-3 items-center text-center text-white rotate-[-3deg] pb-3">
         Статистика
       </div>
