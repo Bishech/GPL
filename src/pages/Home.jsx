15bff28dd2b98a055a79dcebc99055b9f67c8dbb
Bishech <bishechhh@mail.ru>
2024-03-28 22:12:39 +0300

    add TournamentGrid in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,11 @@ import { Footer } from "../components/Footer";
 import { Loader } from "../components/UI/Loader/Loader";
 import { useUnit } from "effector-react";
 import { $teams, fetchTeamsFx } from "../stores/teams";
+import { TournamentGrid } from "../components/TournamentGrid";
+
+// import { Stream } from "../components/Stream";
+// import { Link } from "react-router-dom";
+// import { $userToken } from "../stores/user";
 
 export const Home = () => {
   const teams = useUnit($teams);
@@ -25,8 +30,24 @@ export const Home = () => {
     fetchTeamsFx();
   }, []);
 
+  // const token = useUnit($userToken);
+
   return (
-    <div className="flex flex-col gap-20 mb-20">
+    <div className="flex flex-col items-center gap-20 mb-20">
+      {/* {!token ? (
+        <div className="lg:text-4xl text-2xl text-slate-100 text-center drop-shadow-[0_2px_2px_rgba(0,0,0,1)]">
+          *Для прогнозов на предстоящие матчи необходимо пройти авторизацию
+        </div>
+      ) : (
+        <div className="flex justify-center items-center">
+          <button className="md:w-1/3 w-1/2 p-4 text-lg sm:px-8 sm:text-xl skew-x-[-5deg] font-extra h-14 bg-neutral-900 hover:bg-neutral-800 border-2 rounded-md shadow-md shadow-black border-yellow-400 text-white flex justify-center items-center mr-10 sm:mr-14">
+            <Link to={`/votes`}>
+              <div>Перейти к прогнозам</div>
+            </Link>
+          </button>
+        </div>
+      )} */}
+      <TournamentGrid />
       {/* <div className="flex items-center justify-center object-contain">
         <Stream />
       </div> */}
