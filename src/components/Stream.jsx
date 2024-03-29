ac2a778164d9cc6c3cd0a3fc3bcad0182b80ded9
Bishech <bishechhh@mail.ru>
2024-03-29 13:46:56 +0300

    fix videoId and add classes in Stream

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -1,26 +1,20 @@
-// import { TwitchEmbed } from "react-twitch-embed";
-
-// import YouTube from "react-youtube";
+import YouTube from "react-youtube";
 
 export const Stream = () => {
-  // const opts = {
-  //   height: "450",
-  //   width: "700",
-  //   playerVars: {
-  //     autoplay: 0,
-  //   },
-  // };
-  // const onPlayerReady = (event) => {
-  //   event.target.pauseVideo();
-  // };
-  // return (
-  //   <YouTube opts={opts} onReady={onPlayerReady} videoId={"vxU8i7d6ues"} />
-  // Twitch
-  // <TwitchEmbed
-  //   channel="krifor69"
-  //   autoplay={false}
-  //   hideControls
-  //   withChat={false}
-  // />
-  // );
+  const opts = {
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+  const onPlayerReady = (event) => {
+    event.target.pauseVideo();
+  };
+  return (
+    <YouTube
+      opts={opts}
+      onReady={onPlayerReady}
+      className="youtubeContainer"
+      videoId="JatZmzsBBdc"
+    />
+  );
 };
