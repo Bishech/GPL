2aa18d5649b967bfad3e70d6b082ed145ddfcff9
Bishech <bishechhh@mail.ru>
2024-03-29 23:33:29 +0300

    fix TournamentGrid

--- src/pages/TournamentGrid.jsx
+++ src/pages/TournamentGrid.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import tournamentGrid from "../assets/tournament-grid.png";
 
 export const TournamentGrid = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex justify-center items-center md:w-2/3 w-full h-full">
-        <img src={tournamentGrid} className="block m-auto" />
+        <img
+          src="https://static.gpltournaments.xyz/grid.png"
+          className="block m-auto"
+        />
       </div>
     </div>
   );
